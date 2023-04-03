f85bda87d68f116d220cd2702058b82261ecbae0
muhamaddafa <gaming102dafa@gmail.com>
2023-04-03 17:55:38 +0700

    Update Minigame yang sangat sulit ini

--- game.js
+++ game.js
@@ -2,44 +2,38 @@ var char = document.querySelector("#char");
 var star = document.querySelector("#star");
 var box = document.querySelector("#gameBox");
 
-window.addEventListener('load', () => {
-    char.style.left = 0;
-    char.style.top = 0;
-    });
+window.addEventListener("load", () => {
+  char.style.left = 0;
+  char.style.top = 0;
+});
 
-function checkForCollision(){
-
-}
+function checkForCollision() {}
 
 function moveUp() {
-    char.style.top = parseInt(char.style.top) - 10 + 'px';
+  char.style.top = parseInt(char.style.top) - 10 + "px";
 }
-  
+
 function moveDown() {
-    char.style.top = parseInt(char.style.top) + 10 + 'px';
+  char.style.top = parseInt(char.style.top) + 10 + "px";
 }
-  
+
 function moveLeft() {
-    char.style.left = parseInt(char.style.left) - 10 + 'px';
+  char.style.left = parseInt(char.style.left) - 10 + "px";
 }
-  
+
 function moveRight() {
-    char.style.left = parseInt(char.style.right) + 10 + 'px';
+  char.style.left = parseInt(char.style.left) + 10 + "px";
 }
 
-function clearmove() {
-    myGamePiece.speedX = 0; 
-    myGamePiece.speedY = 0; 
-}
-
-function finish(){
-    window.location.href = "play.html";
+function finish() {
+  window.location.href = "play.html";
 }
 
 const btnSound = document.getElementById("btnSound");
 const audio = document.getElementById("audio");
 
-audio.volume = 0.15;s
+audio.volume = 0.15;
+
 btnSound.addEventListener("click", function () {
   if (audio.paused) {
     audio.play();
